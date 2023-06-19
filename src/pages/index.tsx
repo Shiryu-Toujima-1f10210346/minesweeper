9dc3fae0c292b4531d631f76f387258490921d64
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-06-19 22:20:10 +0900

    You can set some flags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,23 +24,44 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const board = [
+  // -1 ->stone
+  // 0 -> 空
+  // 1 ~ 8 -> 周りの爆弾の数
+  // 9 -> ？
+  // 10 -> flag
+  // 11 -> bomb
+  const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
-    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
-    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
-    [0, 0, 0, 0, 1, 0, 0, 0, 0], // 3
-    [0, 0, 0, 0, 1, 0, 0, 0, 0], // 4
+    [0, 1, 2, 3, 4, 5, 6, 7, 8], // 1
+    [0, 0, 0, 9, 10, 11, 0, 0, 0], // 2
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 4
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
-  ];
-  const onClickCell = (x: number, y: number) => {
-    console.log('hoge');
-    const newInputs = [...userInputs];
-    newInputs[y][x] = 1;
-    setUsetInputs(newInputs);
+  ]);
+  const onClickCell = (x: number, y: number, event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (event.button === 2) {
+      setFlag(x, y);
+    } else {
+      openCell(x, y);
+    }
   };
+
+  const setFlag = (x: number, y: number) => {
+    setBoard((prev) => {
+      const newBoard = [...prev];
+      newBoard[y][x] = 10;
+      return newBoard;
+    });
+  };
+
+  const openCell = (x: number, y: number) => {
+    console.log('openCell', x, y);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -48,9 +69,13 @@ const Home = () => {
           {board.map((row, y) =>
             row.map((cell, x) => (
               <div
-                className={cell === 0 ? styles.cell : styles.voidCell}
+                className={styles.cell}
+                style={{
+                  backgroundPosition: `${(cell - 1) * -30}px 0`,
+                }}
                 key={`${x}-${y}`}
-                onClick={() => onClickCell(x, y)}
+                onClick={(event) => onClickCell(x, y, event)}
+                onContextMenu={(event) => onClickCell(x, y, event)}
               />
             ))
           )}
