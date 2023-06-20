affd327dbf9b8e0ddf6ab329eb96eb5528046c51
Shiryu-Toujima-1f10210346 <Shiryu-Toujima-1f10210346@users.noreply.github.com>
2023-06-20 16:14:05 +0000

    deploy: 90212b53571a4cb81bde3dd6547da3b0ddc81685

---
+++ _next/static/2BZib0RuxpZd71FSZMiNC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a738655812fb1ef8.css","static/chunks/pages/index-c46d46551b34b84b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
