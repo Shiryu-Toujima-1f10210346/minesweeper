addafa9b4995c2a72bb36d9f8bf3974eaa35d6e7
Shiryu-Toujima-1f10210346 <Shiryu-Toujima-1f10210346@users.noreply.github.com>
2023-06-20 13:05:45 +0000

    deploy: 8045825565b0fee202ce02e9d985b9969986c771

---
+++ _next/static/cH5qMrnIjIK5qCcgDalOd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a2cc416ee6b7b0e6.css","static/chunks/pages/index-4536529fd4ca884f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/rOVeat8eJNdw3kxEW790E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/718c2b6d24a687df.css","static/chunks/pages/index-64fb523fefd38b2b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
