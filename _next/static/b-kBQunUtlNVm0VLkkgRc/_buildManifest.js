d055ecac45f2ac0ff43cc5663505058dd196098a
Shiryu-Toujima-1f10210346 <Shiryu-Toujima-1f10210346@users.noreply.github.com>
2023-06-22 08:44:40 +0000

    deploy: 370fca11d9a3863edac3dc23b7fd6f7ea460fb0d

--- _next/static/b-kBQunUtlNVm0VLkkgRc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/34be6275d23b361c.css","static/chunks/pages/index-add9eb8ef1d10301.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
