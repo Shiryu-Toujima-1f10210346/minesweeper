b28bdda87deaef9292fb80aaca03504c9d2c50b5
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-06-22 17:34:16 +0900

    周りの爆弾の数表示できるようになった！！！！

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 7
     [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 8
   ]);
-  const bombCount = 10;
   const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,6 +23,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const bombCount = 10;
   // -1 ->stone
   // 0 -> 空
   // 1 ~ 8 -> 周りの爆弾の数
@@ -32,15 +32,78 @@ const Home = () => {
   // 11 -> bomb
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
-    [1, 2, 3, 4, 5, 6, 7, 8, 0], // 1
-    [1, 2, 3, 4, 5, 6, 7, 8, 0], // 2
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 2
     [0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 4
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 5
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 6
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 7
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1], // 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 4
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
   ]);
+
+  const direction = [
+    [-1, -1], //左上
+    [0, -1], //上
+    [1, -1], //右上
+    [1, 0], //右
+    [1, 1], //右下
+    [0, 1], //下
+    [-1, 1], //左下
+    [-1, 0], //左
+  ];
+  //bombMapを参照しdirectionを使って周りの爆弾の数を計算してboardにセットする
+  const setBombCount = (): void => {
+    const newBoard: number[][] = [...board]; // Create a copy of the board
+
+    for (let row = 0; row < bombMap.length; row++) {
+      for (let col = 0; col < bombMap[row].length; col++) {
+        if (bombMap[row][col] === 11) {
+          // If a bomb is found at current position
+          for (const [dx, dy] of direction) {
+            const newRow = row + dy;
+            const newCol = col + dx;
+
+            if (
+              newRow >= 0 &&
+              newRow < bombMap.length &&
+              newCol >= 0 &&
+              newCol < bombMap[row].length &&
+              bombMap[newRow][newCol] !== 11
+            ) {
+              // Increment the count in the corresponding board cell
+              newBoard[newRow][newCol]++;
+            }
+          }
+        }
+      }
+    }
+
+    setBoard(newBoard); // Update the board with the new counts
+    console.log('↓BombCount↓');
+    console.table(board);
+  };
+
+  //boardにbombMapをセットする
+
+  const mergeMap = (): number[][] => {
+    const newBoard: number[][] = [];
+
+    for (let row = 0; row < Math.max(bombMap.length, board.length); row++) {
+      const newRow: number[] = [];
+      for (let col = 0; col < Math.max(bombMap[row]?.length || 0, board[row]?.length || 0); col++) {
+        const bombValue = bombMap[row]?.[col] || 0;
+        const boardValue = board[row]?.[col] || 0;
+        newRow.push(bombValue === 11 ? bombValue : boardValue);
+      }
+      newBoard.push(newRow);
+    }
+
+    setBoard(newBoard); // Update the board with the new counts
+    console.log('↓Merged Map↓');
+    console.table(board);
+  };
+
   const onClickCell = (x: number, y: number, event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
     if (event.button === 2) {
@@ -67,6 +130,8 @@ const Home = () => {
       newBombMap[bombY][bombX] = 11;
     }
     setBombMap(newBombMap);
+    setBombCount();
+    mergeMap();
     console.log('↓bombMap↓');
     console.table(bombMap);
   };
@@ -124,7 +189,7 @@ const Home = () => {
           <div className={styles.gameover} />
         </div>
         <div className={styles.board}>
-          {bombMap.map((row, y) =>
+          {board.map((row, y) =>
             row.map((cell, x) => (
               <div
                 className={styles.cell}
